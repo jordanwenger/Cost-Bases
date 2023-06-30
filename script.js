bb89ef98384a2df955fa11d7353b7a8f1a14ec87
Jordan <jordanwenger11@gmail.com>
2023-06-29 20:30:19 -0400

    added slider menu for mobile and footer

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cardOne = document.getElementById('card-1-price');
 const cardTwo = document.getElementById('card-2-price');
 const cardThree = document.getElementById('card-3-price')
+const hamburgerBtn = document.getElementById('hamburger')
+const menuTab = document.getElementById('menu-tab')
+const closeBtn = document.getElementById('close-tab')
 
 function getPrices() {
     getFuboPrice();
@@ -9,25 +12,13 @@ function getPrices() {
 }
 
 function getFuboPrice() {
-    fetch('https://api.polygon.io/v2/aggs/ticker/FUBO/prev?adjusted=true&apiKey=key')
-        .then((results) => results.json())
-        .then((data) => {
-            cardOne.innerHTML = `$${data.results[0].c}`
-        })
-}
-function getGMEPrice() {
-    fetch('https://api.polygon.io/v2/aggs/ticker/GME/prev?adjusted=true&apiKey=key')
-        .then((results) => results.json())
-        .then((data) => {
-            cardTwo.innerHTML = `$${data.results[0].c}`
-        })
-}
-function getBTUPrice() {
-    fetch('https://api.polygon.io/v2/aggs/ticker/BTU/prev?adjusted=true&apiKey=key')
-        .then((results) => results.json())
-        .then((data) => {
-            cardThree.innerHTML = `$${data.results[0].c}`
-        })
+    
 }
 
-// window.addEventListener('DOMContentLoaded', getPrices)
+function initiatePopup() {
+    menuTab.classList.toggle('is-active')
+}
+
+window.addEventListener('DOMContentLoaded', getPrices);
+hamburgerBtn.addEventListener('click', initiatePopup);
+closeBtn.addEventListener('click', initiatePopup)
