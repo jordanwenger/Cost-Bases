b8030782e348a3cb2a614c1cfe5c1902bfe3216f
Jordan <jordanwenger11@gmail.com>
2023-07-07 21:53:22 -0400

    updated stock card design

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ function getPrices() {
 }
 
 function getFuboPrice() {
-    fetch('https://api.polygon.io/v2/aggs/ticker/FUBO/prev?adjusted=true&apiKey=')
+    fetch('https://api.polygon.io/v2/aggs/ticker/FUBO/prev?adjusted=true&apiKey=F1jzPnyRpp01J6caAkMd4o2NYXHdn9Dm')
         .then((results) => results.json())
         .then((data) => {
             cardOne.innerHTML = `$${data.results[0].c}`
         })
 }
 function getGMEPrice() {
-    fetch('https://api.polygon.io/v2/aggs/ticker/GME/prev?adjusted=true&apiKey=')
+    fetch('https://api.polygon.io/v2/aggs/ticker/GME/prev?adjusted=true&apiKey=F1jzPnyRpp01J6caAkMd4o2NYXHdn9Dm')
         .then((results) => results.json())
         .then((data) => {
             cardTwo.innerHTML = `$${data.results[0].c}`
         })
 }
 function getBTUPrice() {
-    fetch('https://api.polygon.io/v2/aggs/ticker/BTU/prev?adjusted=true&apiKey=')
+    fetch('https://api.polygon.io/v2/aggs/ticker/BTU/prev?adjusted=true&apiKey=F1jzPnyRpp01J6caAkMd4o2NYXHdn9Dm')
         .then((results) => results.json())
         .then((data) => {
             cardThree.innerHTML = `$${data.results[0].c}`
